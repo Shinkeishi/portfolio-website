c4a34b109320ecb000ff3b8cf1e78c6b9f5aa1f2
Shinkeishi <ashleyytanjx@gmail.com>
2024-01-16 02:04:37 +0800

    Updated files for GH deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,4 @@ import react from "@vitejs/plugin-react";
 
 export default {
     plugins: [react()],
-    build: {
-        rollupOptions: {
-            input: "src/main.jsx",
-        },
-    },
 };
